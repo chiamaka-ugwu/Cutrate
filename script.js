2810528ea69c55a4c9eacba7389e38f4ad90c3a8
Chiamaka Ugwu <mannehills@gmail.com>
2022-05-12 16:29:14 +0100

    Update

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ var menuIcon = document.getElementById("menu-icon");
 var close = document.getElementById("close");
 var dropDown = document.getElementById("dropdown");
 var body = document.getElementById("body");
-    
+var nav = document.getElementById("nav");
+var links = document.querySelectorAll(".link");
+   
 
 
 menuIcon.addEventListener("click", function(){    
     dropDown.style.display = "block";
     menuIcon.style.display = "none";
     close.style.display = "block";
-    body.style.filter = "contrast(85%)";
+    body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    body.style.zIndex = "1000";
+    nav.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+
 })
 
 
@@ -17,14 +22,39 @@ close.addEventListener("click", function(){
     dropDown.style.display = "none";
     menuIcon.style.display = "block";
     close.style.display = "none";
-    body.style.filter = "contrast(100%)";
+    body.style.backgroundColor = "rgba(50, 12, 94, 0.2)";
+    body.style.zIndex = "1";
+    nav.style.backgroundColor = "#fff";
+
+
 })
 
-document.onclick = function(e){
-    if(e.target.id !== "dropdown" && e.target.id !== "menu-icon"){
-        dropDown.style.display = "none";
-        menuIcon.style.display = "block";
-        close.style.display = "none";
-        body.style.filter = "contrast(100%)";
+
+const x = window.matchMedia("(max-width: 1000px)");
+if(x.matches){
+    document.onclick = function(e){
+        if(e.target.id !== "dropdown" && e.target.id !== "menu-icon"){
+            dropDown.style.display = "none";
+            menuIcon.style.display = "block";
+            close.style.display = "none";
+            body.style.backgroundColor = "rgba(50, 12, 94, 0.2)";
+            body.style.zIndex = "1";
+            nav.style.backgroundColor = "#fff";
+    
+        }
     }
+}
+
+
+const active = window.matchMedia("(max-width: 2000px)");
+if(active.matches){
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) { 
+          current[0].className = current[0].className.replace("active", "");
+        }
+        this.className += " active";
+        });
+      }
 }
